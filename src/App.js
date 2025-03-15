eb080f2132ad3e4a17cc5ef12da9ea724f38a67d
prathameshdadgade <prathameshdagade@gmail.com>
2025-03-15 14:00:03 +0530

    https://github.com/prathameshdadgade/Doc-Manger.git

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DocumentManager from "./DocumentManager";
+
+function App() {
+  return (
+    <div>
+      <h1>Document Management System</h1>
+      <DocumentManager />
+    </div>
+  );
+}
+
+export default App;
